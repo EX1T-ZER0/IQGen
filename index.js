dfddbde0c90ede100f15630268b111aaff9dd2e7
EX1T-ZER0 <mdsajidhakhand@gmail.com>
2020-08-11 17:53:33 +0600

    lesgo

---
+++ index.js
@@ -0,0 +1,18 @@
+var PORT = process.env.PORT || 5000;
+const exp = require('express');
+const iq = require('inspirational-quotes');
+const app = exp();
+
+
+
+
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+    let quote = iq.getQuote();
+    res.render('index.ejs',{
+        qt : quote.text,
+        au : quote.author
+    });
+})
+
+app.listen(PORT, () => console.log("Initialized"));
